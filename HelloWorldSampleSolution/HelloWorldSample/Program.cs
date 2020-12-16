43ac1500669f4d2c844ba7e73233f2467a64dc63
Sowmya <sowmya.ramesh@lntinfotech.com>
2020-12-16 09:28:42 +0530

    Hello world updated

--- HelloWorldSampleSolution/HelloWorldSample/Program.cs
+++ HelloWorldSampleSolution/HelloWorldSample/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorldSample
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome");
             Console.ReadKey();
 
         }
